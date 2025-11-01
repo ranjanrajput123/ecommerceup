82b0e98bcfad796ff728b0d03ca98434523d07e3
ranjanrajput123 <rajputranjan1996.com>
2025-11-01 21:35:46 +0530

    updated index.blade.php design

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -11,9 +11,11 @@
                         <h1 class="mb-5 display-3 text-primary">Organic Veggies & Fruits Foods</h1>
                         <div class="position-relative mx-auto">
                             <input class="form-control border-2 border-secondary w-75 py-3 px-4 rounded-pill" type="number" placeholder="Search">
-                            <button type="submit" class="btn btn-primary border-2 border-secondary py-3 px-4 position-absolute rounded-pill text-white h-100" style="top: 0; right: 25%;">Submit Now</button>
+                            <button type="submit" class="btn btn-primary border-2 border-secondary py-3 px-4 position-absolute rounded-pill text-white h-100" style="top: 0; right: 25%;">Submit Ranjan Now</button>
                         </div>
                     </div>
+
+                    
                     <div class="col-md-12 col-lg-5">
                         <div id="carouselId" class="carousel slide position-relative" data-bs-ride="carousel">
                             <div class="carousel-inner" role="listbox">
